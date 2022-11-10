bde26781cb15893c25ddcdee43eaecdf364c7403
Brad Jones <BJ2044@gmail.com>
2022-11-10 14:47:14 -0500

    Working

--- typescript.ts
+++ typescript.ts
@@ -1,44 +1,64 @@
 //console.log("Hello typescript")
 
-let firstName: string = "Brad"
-let age: number = 30
-let good: boolean = true
+// let firstName: string = "Brad"
+// let age: number = 30
+// let good: boolean = true
 
-// firstName = 4 (Not Valid)
+// // firstName = 4 (Not Valid)
 
-let fruits: string[] = [
-    "banana",
-    "apple"
-]
+// let fruits: string[] = [
+//     "banana",
+//     "apple"
+// ]
 
-console.log(`
-    First Name: ${firstName}
-    Your Age: ${age}
-    You Have Many: ${fruits}
-`)
+// console.log(`
+//     First Name: ${firstName}
+//     Your Age: ${age}
+//     You Have Many: ${fruits}
+// `)
 
 
-enum taxForm {
-    standard = "1040",
-    childCredit = "1065",
-    SCorp = "1121",
-    CCorp = "1120s",
+// enum taxForm {
+//     standard = "1040",
+//     childCredit = "1065",
+//     SCorp = "1121",
+//     CCorp = "1120s",
+// }
+
+//  console.log(`
+//     You Need Form ${taxForm.childCredit} 
+// `)
+
+
+// function getFruit( index: number): string {
+
+//     return fruits[index]
+// }
+
+// function debug(message: string): void {
+//     console.log(message)
+// }
+
+// debug("OMG NO RETURN")
+
+// console.log(` You choose an ${getFruit(0)}`)
+
+// let chosenFruit: string = getFruit(0)
+
+// debug(` You choose an ${getFruit(0)})
+
+interface teacher {
+    name: string,
+    years: number,
+    subject: string[],
+    age: Number
 }
 
-console.log(`
-    You Need Form ${taxForm.childCredit} 
-`)
-
-
-function getFruit( index: number): string {
-
-    return fruits[index]
+let brad: teacher = {
+    name: "brad",
+    years: 10,
+    subject:["javascript", "typescript"],
+    age: 30,
 }
 
-function debug(message: string): void {
-    console.log(message)
-}
-
-debug("OMG NO RETURN")
-
-console.log(` You choose an ${getFruit(0)}`)
+console.log(brad)
